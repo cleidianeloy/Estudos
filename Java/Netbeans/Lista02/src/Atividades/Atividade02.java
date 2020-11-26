17e07191b0360583ec63d9c5a7291916d23f42db
Cleidiane Eloy <cleidiane.re@aluno.ifsc.edu.br>
2020-11-26 13:31:36 -0300

    arquivos atualizados

--- Java/Netbeans/Lista02/src/Atividades/Atividade02.java
+++ Java/Netbeans/Lista02/src/Atividades/Atividade02.java
@@ -32,7 +32,7 @@ public class Atividade02 {
         }
         if(numeros[0] < numeros[1])
         {
-            System.out.println(numeros[1] + " é o maior");
+            System.out.println(numeros[1] + " é o menor");
         }
         if(numeros[0] == numeros[1]){
             System.out.println("os dois numeros são iguais");
